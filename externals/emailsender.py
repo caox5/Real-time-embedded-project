8de9e60ad3ccf7dc5a6c28b42f1c51df2c3ce6c9
caox5 <50026800+caox5@users.noreply.github.com>
2022-04-19 09:28:39 +0100

    send email function

---
+++ externals/emailsender.py
@@ -0,0 +1,37 @@
+import smtplib
+
+#Email Variables
+SMTP_SERVER = 'smtp.gmail.com' #Email Server (don't change!)
+SMTP_PORT = 587 #Server Port (don't change!)
+GMAIL_USERNAME = 'falldetectionrpi@gmail.com' #change this to match your gmail account
+GMAIL_PASSWORD = 'Caoxin12345' #change this to match your gmail password
+
+class Emailer:
+    def sendmail(self, recipient, subject, content):
+
+#Create Headers
+        headers = ["From: " + GMAIL_USERNAME, "Subject: " + subject, "To: " + recipient,
+                   "MIME-Version: 1.0", "Content-Type: text/html"]
+        headers = "\r\n".join(headers)
+
+#Connect to Gmail Server
+        session = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        session.ehlo()
+        session.starttls()
+        session.ehlo()
+
+#Login to Gmail
+        session.login(GMAIL_USERNAME, GMAIL_PASSWORD)
+
+#Send Email & Exit
+        session.sendmail(GMAIL_USERNAME, recipient, headers + "\r\n\r\n" + content)
+        session.quit
+
+sender = Emailer()
+
+sendTo = 'loowing397@163.com' #Change this to the email you wish to send to
+emailSubject = "Falldetector" #The Subject of the Email
+emailContent = "Fall has been detected!!!" #The Content / Body of the email
+
+#Sends an email to the "sendTo" address with the specified "emailSubject" as the subject and "emailContent" as the email content.
+sender.sendmail(sendTo, emailSubject, emailContent)
