468d561a4c65feaf1336d5a239a0f3ab3b8b4e5e
caox5 <50026800+caox5@users.noreply.github.com>
2022-04-19 09:40:40 +0100

    test for camera

---
+++ test/video.cpp
@@ -0,0 +1,28 @@
+// cam test program
+#include "opencv2/opencv.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include <opencv2/imgproc/imgproc.hpp>
+#include <tuple>
+
+using namespace cv;
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    Mat frame;
+    VideoCapture cap;
+    // open the default camera, use something different from 0 otherwise;
+    // Check VideoCapture documentation.
+    if(!cap.open(0))
+        return 0;
+    for(;;)
+    {
+          cap >> frame;
+          if( frame.empty() ) break; // end of video stream
+          imshow("Camera", frame);
+          if( (waitKey(10)%256) == 27 ) break; // waitKeypatch: check for 10ms: then stop capturing by pressing ESC=27
+    }
+    // the camera will be closed automatically upon exit
+    //cap.close();
+    return 0;
+}
