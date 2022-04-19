929bfd85dd2f517443879d87f2650f78b86de3cf
2646944C <103906407+2646944C@users.noreply.github.com>
2022-04-19 22:58:31 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -26,21 +26,21 @@ int main() {
 
     Mat frame, eroded, dilated;
     Mat backgroundImg, foregroundImg; //"foregroundImg" is the moving object (the human body that will be tracked)
-    Mat fgMaskMOG2;//
-    Mat history;
+    Mat fgMaskMOG2; //A specific region in a frame
+    Mat history; //The collection of information from multiple frames(fgMaskMOG2)
 
     Ptr<BackgroundSubtractorMOG2> pMOG2 = createBackgroundSubtractorMOG2();
 
-    vector<vector<Point> > contours;
-    vector<Vec4i> hierarchy;
-    vector<Point> largestContour;
+    vector<vector<Point> > contours; //The collection of contour points of the human body
+    vector<Vec4i> hierarchy; //The format of each element in contours
+    vector<Point> largestContour; //The dataset of the largest contour. When multiple moving objects are detected, only the largest object will be tracked and determined if it falls
     vector<Moments> center(1);
 
-    vector<double> thetaRatio;
-    vector<double> aRatio;
-    vector<double> bRatio;
-    vector<double> xPos;
-    vector<double> yPos;
+    vector<double> thetaRatio; //The angle of theta of the ellipse
+    vector<double> aRatio; //The long axis of the ellipse
+    vector<double> bRatio; //The short axis of the ellipse
+    vector<double> xPos;  
+    vector<double> yPos; //The coordinates of the center point of the ellipse
 
     double movementCoefficientValue;
     double aRatioValue;
